221a94d1fd62fac0d0a775f2d762ddb302473d44
javadbat <javadbat@gmail.com>
2024-12-18 16:15:57 +0330

    fix validation error on empty input

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,6 +3,10 @@ import { type JBInputValue } from "jb-input";
 import {isStringIsNumber} from './utils';
 export const isNumberValidator :ValidationItem<JBInputValue> = {
   validator:({value})=>{
+    // we allow empty value as an valid number
+    if(value.trim().length == 0){
+      return true;
+    }
     return isStringIsNumber(value);
   },
   message:"مقدار وارد شده میبایست عدد باشد"
